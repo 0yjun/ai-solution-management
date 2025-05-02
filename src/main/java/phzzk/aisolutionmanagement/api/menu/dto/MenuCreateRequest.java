45acec872cd533171f17563eb8d2447d8fae2b3b
0jun <an0yjun@gmail.com>
2025-05-02 21:10:47 +0900

    메뉴 추가 양식 변경

--- src/main/java/phzzk/aisolutionmanagement/api/menu/dto/MenuCreateRequest.java
+++ src/main/java/phzzk/aisolutionmanagement/api/menu/dto/MenuCreateRequest.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import phzzk.aisolutionmanagement.common.constants.Role;
 
+import java.util.List;
 import java.util.Set;
 
 @Schema(name = "MenuCreateRequest", description = "메뉴 생성 요청 DTO")
@@ -16,26 +17,29 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MenuCreateRequest {
-
-    @Schema(description = "메뉴명", example = "Dashboard", required = true)
-    @NotBlank(message = "메뉴 이름을 입력해주세요.")
+    @Schema(description = "메뉴명", example = "Dashboard")
     private String name;
 
-    @Schema(description = "설명", example = "관리자 대시보드 화면", required = false)
-    @Size(max = 255, message = "설명은 최대 255자까지 입력 가능합니다.")
+    @Schema(description = "메뉴설명", example = "Dashboard")
     private String description;
 
-    @Schema(description = "부모 메뉴 ID (최상위일 경우 null)", example = "1", required = false)
-    private Long parentId;
-
-    @Schema(description = "URL (슬래시(`/`)로 시작)", example = "/dashboard", required = true)
-    @NotBlank(message = "URL을 입력해주세요.")
-    @Pattern(regexp = "^/.*", message = "URL은 '/'로 시작해야 합니다.")
+    @Schema(description = "URL", example = "/dashboard")
     private String url;
 
-    @Schema(description = "아이콘명 (예: material icon 이름)", example = "dashboard", required = true)
-    @NotBlank(message = "아이콘을 입력해주세요.")
+    @Schema(description = "메뉴순서", example = "1")
+    private Integer seq;
+
+    @Schema(description = "아이콘명", example = "dashboard")
     private String icon;
+
+    @Schema(description = "활성화 여부", example = "true | false")
+    private boolean isActive;
+
+    @Schema(description = "부모 메뉴 ID (null일 수 있음)", example = "1")
+    private Long parentId;
+
+    @Schema(description = "하위 메뉴 목록")
+    private List<MenuAdminDto> children;
 
     @Schema(
             description = "허용된 Role 목록 (최소 1개, 최대 4개)",
@@ -48,4 +52,22 @@ public class MenuCreateRequest {
             message = "권한은 최대 {max}개까지 선택 가능합니다."
     )
     private Set<@NotNull Role> roles;
+
+    @Schema(description = "이전 메뉴 ID", example = "1")
+    private Long prevMenuId;
+
+    @Schema(description = "이전 메뉴 url", example = "/prev")
+    private String prevMenuUrl;
+
+    @Schema(description = "이전 메뉴 이름", example = "이전메뉴")
+    private String prevMenuName;
+
+    @Schema(description = "다음 메뉴 ID", example = "1")
+    private Long nextMenuId;
+
+    @Schema(description = "다음 메뉴 url", example = "/next")
+    private String nextMenuUrl;
+
+    @Schema(description = "다음 메뉴 이름", example = "다음 메뉴")
+    private String nextMenuName;
 }
